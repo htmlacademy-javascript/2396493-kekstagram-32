833ae081ce04e146cd5a35e215a17fd369f50158
ilyta45 <savin.ilya4518061993@gmail.com>
2024-06-27 20:00:43 +0500

    Создает функцию для проверки строки, является ли она палиндромом

--- js/functions.js
+++ js/functions.js
@@ -3,3 +3,13 @@
 const checkRandomString = (string, length) => string.length <= maxLength;
 
   checkRandomString();
+
+// Функция для проверки строки, является ли она палиндромом
+const checkPalindrome = (string) => {
+  const cleaned = string.replaceAll(' ', '').toLowerCase();
+  const stringNormalize = cleaned.split('').reverse().join('');
+
+  return cleaned === stringNormalize;
+};
+
+checkPalindrome();
